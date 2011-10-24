80998d7992ba5fb563b01745b2b3b7cd2875a516
AceLan Kao <acelan.kao@canonical.com>
2011-10-24 11:12:21 +0800

    lfdd: fix ioctl issue
    The ioctl function already be removed from file_operations struct since
    2.6.36 kernel. So, replace it by unlocked_ioctl function.
    
    Signed-off-by: AceLan Kao <acelan.kao@canonical.com>

--- lfdd/lfdd.c
+++ lfdd/lfdd.c
@@ -94,7 +94,7 @@ static int lfdd_release( struct inode *inode, struct file *file ) {
 }
 
 
-static int lfdd_ioctl( struct inode *inode, struct file *file
+static int lfdd_ioctl( struct file *file
                             , unsigned int cmd, unsigned long arg ) {
 
     struct lfdd_pci_t lfdd_pci_data;
@@ -203,7 +203,7 @@ static int lfdd_ioctl( struct inode *inode, struct file *file
 static struct file_operations lfdd_fops = {
 
     .owner      =   THIS_MODULE,
-    .ioctl      =   lfdd_ioctl,
+    .unlocked_ioctl =   lfdd_ioctl,
     .open       =   lfdd_open,
     .release    =   lfdd_release,
 };
